e51d1f26b63e13f0f9ad4c762fc38f578656858a
smsanchez1 <smsanchez@typsa.es>
2025-02-06 08:53:18 +0100

    feat(first commit)
    this is the first commit, uploading the 42 header for visual studio template

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# Unknown-Interactive-header
-Unknown-Interactive-header temporal name and header for visual studio
+<img
+  src="https://raw.githubusercontent.com/kube/vscode-42header/master/42.png" 
+  width=128>
+
+> **Searching for Maintainer.**
+> 
+> This project was done 7 years ago, which is an eternity relative to VSCode Lifespan.
+> I don't have time to maintain this, but I see a lot of people doing Pull Requests for it, or creating issues.
+> 
+> As there are no Unit Tests and no CI/CD, it makes it even harder to check for issues and PRs.
+>
+> What I'd like to do is to give ownership to an "official" 42 organization on GitHub, where a group of students could maintain and update this project themselves.
+>
+> If you are interested in such a thing, please ping me.
+
+# 42 Header for VSCode
+
+This extension provides the 42 header integration in VS Code.
+
+```bash
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    vscode-42header                                    :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: kube <hello@kube.io>                       +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2013/11/18 13:37:42 by kube              #+#    #+#              #
+#    Updated: 2016/09/18 13:11:04 by kube             ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+```
+
+## Install
+
+Launch Quick Open with <kbd>⌘</kbd>+<kbd>P</kbd> and enter
+```
+ext install 42header
+```
+
+## Usage
+
+### Insert a header
+ - **macOS** : <kbd>⌘</kbd> + <kbd>⌥</kbd> + <kbd>H</kbd>
+ - **Linux** / **Windows** : <kbd>Ctrl</kbd> + <kbd>Alt</kbd> + <kbd>H</kbd>.
+
+Header is automatically updated on save.
+
+
+## Configuration
+
+Default values for **username** and **email** are imported from environment variables.
+
+To override these values, specify these properties in *User Settings* :
+
+```ts
+{
+  "42header.username": string,
+  "42header.email": string
+}
+```
+
+
+## Issues
+
+In case of a bug, or missing feature, please create a [Github Pull Request](https://github.com/kube/vscode-42header/pulls).
+
+## License
+
+MIT
---
+++ src/extension.ts
@@ -0,0 +1,130 @@
+
+      /*#######.
+     ########",#:
+   #########',##".
+  ##'##'## .##',##.
+   ## ## ## # ##",#.
+    ## ## ## ## ##'
+     ## ## ## :##
+      ## ## ##*/
+
+import { basename } from 'path'
+import vscode = require('vscode')
+import moment = require('moment')
+
+import {
+  ExtensionContext, TextEdit, TextEditorEdit, TextDocument, Position, Range
+} from 'vscode'
+
+import {
+  extractHeader, getHeaderInfo, renderHeader,
+  supportsLanguage, HeaderInfo
+} from './header'
+
+/**
+ * Return current user from config or ENV by default
+ */
+const getCurrentUser = () =>
+  vscode.workspace.getConfiguration()
+    .get('42header.username') || process.env['USER'] || 'marvin'
+
+/**
+ * Return current user mail from config or default value
+ */
+const getCurrentUserMail = () =>
+  vscode.workspace.getConfiguration()
+    .get('42header.email') || `${getCurrentUser()}@student.42.fr`
+
+/**
+ * Update HeaderInfo with last update author and date, and update filename
+ * Returns a fresh new HeaderInfo if none was passed
+ */
+const newHeaderInfo = (document: TextDocument, headerInfo?: HeaderInfo) => {
+  const user = getCurrentUser()
+  const mail = getCurrentUserMail()
+
+  return Object.assign({},
+    // This will be overwritten if headerInfo is not null
+    {
+      createdAt: moment(),
+      createdBy: user
+    },
+    headerInfo,
+    {
+      filename: basename(document.fileName),
+      author: `${user} <${mail}>`,
+      updatedBy: user,
+      updatedAt: moment()
+    }
+  )
+}
+
+/**
+ * `insertHeader` Command Handler
+ */
+const insertHeaderHandler = () => {
+  const { activeTextEditor } = vscode.window
+  const { document } = activeTextEditor
+
+  if (supportsLanguage(document.languageId))
+    activeTextEditor.edit(editor => {
+      const currentHeader = extractHeader(document.getText())
+
+      if (currentHeader)
+        editor.replace(
+          new Range(0, 0, 12, 0),
+          renderHeader(
+            document.languageId,
+            newHeaderInfo(document, getHeaderInfo(currentHeader))
+          )
+        )
+      else
+        editor.insert(
+          new Position(0, 0),
+          renderHeader(
+            document.languageId,
+            newHeaderInfo(document)
+          )
+        )
+    })
+  else
+    vscode.window.showInformationMessage(
+      `No header support for language ${document.languageId}`
+    )
+}
+
+/**
+ * Start watcher for document save to update current header
+ */
+const startUpdateOnSaveWatcher = (subscriptions: vscode.Disposable[]) =>
+  vscode.workspace.onWillSaveTextDocument(event => {
+    const document = event.document
+    const currentHeader = extractHeader(document.getText())
+
+    event.waitUntil(
+      Promise.resolve(
+        supportsLanguage(document.languageId) && currentHeader ?
+          [
+            TextEdit.replace(
+              new Range(0, 0, 12, 0),
+              renderHeader(
+                document.languageId,
+                newHeaderInfo(document, getHeaderInfo(currentHeader))
+              )
+            )
+          ]
+          : [] // No TextEdit to apply
+      )
+    )
+  },
+    null, subscriptions
+  )
+
+
+export const activate = (context: vscode.ExtensionContext) => {
+  const disposable = vscode.commands
+    .registerTextEditorCommand('42header.insertHeader', insertHeaderHandler)
+
+  context.subscriptions.push(disposable)
+  startUpdateOnSaveWatcher(context.subscriptions)
+}
